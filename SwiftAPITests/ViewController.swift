2c483a6b9952dccb5c4d980421116f8591c4bbe6
yuanfangysq <ysq19941125@163.com>
2016-06-03 09:25:59 +0800

    检测是否写入成功

--- SwiftAPITests/ViewController.swift
+++ SwiftAPITests/ViewController.swift
@@ -16,12 +16,21 @@ class ViewController: UIViewController {
         let path:NSString = NSBundle.mainBundle().pathForResource("text", ofType: "plist")!
         let infoPlist =  NSArray(contentsOfFile: path as String)
      
-       let intex0:Bool=jiexi(infoPlist![0] as! NSString, index: 0)
-       let intex1:Bool=jiexi(infoPlist![1] as! NSString, index: 1)
-       let intex2:Bool=jiexi(infoPlist![2] as! NSString, index: 2)
+   
         
-           label.text="写人成功"
-          
+        jiexi(infoPlist![0] as! NSString, index: 0) { () -> (Void) in
+           
+            print("第一个写入成功")
+        }
+        jiexi(infoPlist![1] as! NSString, index: 0) { () -> (Void) in
+            print("第二个写入成功")
+        }
+        jiexi(infoPlist![2] as! NSString, index: 0) { () -> (Void) in
+            print("第三个写入成功")
+           
+        }
+        
+       
         
     }
     
@@ -30,8 +39,8 @@ class ViewController: UIViewController {
 
     }
     
-    func jiexi(str:NSString, index:NSInteger) -> Bool {
-        var panduan:Bool=false
+    func jiexi(str:NSString, index:NSInteger,block:()->(Void)) -> Void {
+        
         let manager = AFHTTPRequestOperationManager();
         
         let acceptableTypes = NSMutableSet();
@@ -40,7 +49,7 @@ class ViewController: UIViewController {
         manager.responseSerializer.acceptableContentTypes=acceptableTypes as NSSet as Set<NSObject>;
         manager.GET(str as String, parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
             
-            panduan=true;
+
             let string = (response as! NSDictionary)["result"] as! NSArray
             
             var fm:NSFileManager = NSFileManager.defaultManager()
@@ -51,12 +60,12 @@ class ViewController: UIViewController {
             fm.createFileAtPath(plistPath as String, contents: nil, attributes: nil)
             
             (string[0] as! NSDictionary).writeToFile(plistPath as String, atomically: true)
-            
+            block();
         }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
             return false
             
         };
-        return panduan;
+       
     }
 }
 
